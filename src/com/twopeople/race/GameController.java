5804fa4d9a472a746833c7f815c3373c6eb74565
Alexey Chernikov <chernikov.lesha@gmail.com>
2013-07-10 20:24:05 +0300

    Errors fixed

---
+++ src/com/twopeople/race/GameController.java
@@ -0,0 +1,16 @@
+package com.twopeople.race;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class GameController extends StateBasedGame {
+	public GameController() {
+		super("Space Hackathon");
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+
+	}
+}
---
+++ src/com/twopeople/race/GameStartup.java
@@ -0,0 +1,16 @@
+package com.twopeople.race;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class GameStartup {
+	public static void main(String[] args) {
+		try {
+			AppGameContainer game = new AppGameContainer(new GameController());
+			game.setDisplayMode(800, 600, false);
+			game.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+}
