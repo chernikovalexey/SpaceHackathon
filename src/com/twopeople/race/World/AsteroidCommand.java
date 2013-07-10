e7cca97de0d46e55b72517494a8ebdad291c9cd6
podkolzzzin <1996*zorro>
2013-07-10 22:32:34 +0300

    world parsing part 1

---
+++ src/com/twopeople/race/World/AsteroidCommand.java
@@ -0,0 +1,20 @@
+package com.twopeople.race.World;
+
+import com.twopeople.race.entity.Asteroid;
+import com.twopeople.race.entity.Entity;
+
+/**
+ * Created with podko_000.
+ * User: podko_000
+ * Date: 10.07.13
+ * Time: 22:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class AsteroidCommand implements IWorldColorCommand {
+
+    @Override
+    public Entity execute(World world) {
+        //Asteroid asteroid=new Asteroid();
+          return null;
+    }
+}
---
+++ src/com/twopeople/race/World/IWorldColorCommand.java
@@ -0,0 +1,14 @@
+package com.twopeople.race.World;
+
+import com.twopeople.race.entity.Entity;
+
+/**
+ * Created with podko_000.
+ * User: podko_000
+ * Date: 10.07.13
+ * Time: 22:15
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IWorldColorCommand {
+    public Entity execute(World world);
+}
