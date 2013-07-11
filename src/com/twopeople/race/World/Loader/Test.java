0a337e7455441d2e8595299cf98344e978526fca
podkolzzzin <1996*zorro>
2013-07-11 22:19:12 +0300

    Network part 2

---
+++ src/com/twopeople/race/World/Loader/Test.java
@@ -0,0 +1,20 @@
+package com.twopeople.race.World.Loader;
+
+import com.twopeople.race.World.Camera;
+import com.twopeople.race.World.World;
+import org.newdawn.slick.SlickException;
+
+import java.io.IOException;
+
+/**
+ * Created with podko_000.
+ * User: podko_000
+ * Date: 10.07.13
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SlickException {
+
+    }
+}
